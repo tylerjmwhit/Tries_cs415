1b7da14119e2c3f83982a7eeb32b1c8bb39cdd0c
tylerjmwhit <tylerjmwhit@gmail.com>
2022-04-24 20:50:26 -0700

    changed up reader class to be more streamlined

--- main.cpp
+++ main.cpp
@@ -4,11 +4,16 @@
 #include "Trie.hpp"
 
 int main(int argc, const char *argv[]) {
+	const char *flag = argv[2];
+	if (*flag == '2'){
+		std::cout << "flag is 2";
+	}
 	Trie *_myTrie = new Trie(); // creating the trie
 	std::string mytext, myword; // mytext will read in text fill to just lowercase words seperated by spaces, myword is used to pull words from this
 	Reader myread = Reader(argv[1]); //reading in the text file
 	mytext += myread.gettext(); //reading in the text file
 	std::stringstream ss(mytext);
+
 	while (getline(ss,myword, ' ')){ //insert the text file into the trie one word at a time
 			_myTrie->insert(myword);
 	}
---
+++ treenode.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Tyler on 4/24/2022.
+//
+
+#ifndef CS415PROJECT3_TREENODE_HPP
+#define CS415PROJECT3_TREENODE_HPP
+
+#endif //CS415PROJECT3_TREENODE_HPP
